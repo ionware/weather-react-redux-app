6e9c6f302844c073f9f3d75a06e7b296244cfeed
ionware <ionwarez@gmail.com>
2017-04-27 17:03:12 +0100

    Make the searchBar controlled container

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -2,12 +2,28 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component
 {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            term : ""
+        }
+
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onInputChange(event){
+        this.setState({term : event.target.value});
+    }
+
     render(){
         return (
            <div className='row text-center'>
                <div className='col-md-6'>
                    <form className='input-group'>
-                       <input className='form-control' placeholder='Enter City' />
+                       <input className='form-control' placeholder='Enter City'
+                           onChange={ this.onInputChange }
+                           value={ this.state.term }/>
                         <span className='input-group-btn'>
                             <button className='btn btn-primary'>Search</button>
                         </span>
